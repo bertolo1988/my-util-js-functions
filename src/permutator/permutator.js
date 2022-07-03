90dd0e06a2475e4c9ed1da93046ec68064a7cbeb
bertolo1988 <tiagobertolo@gmail.com>
2022-07-03 23:31:34 +0100

    Improve the permutator.

--- src/permutator/permutator.js
+++ src/permutator/permutator.js
@@ -1,21 +1,28 @@
-function permutator(inputArr) {
-  var results = []
-  function permute(arr, memo) {
-    var cur,
-      memo = memo || []
+// https://stackoverflow.com/questions/9960908/permutations-in-javascript/37580979#37580979
 
-    for (var i = 0; i < arr.length; i++) {
-      cur = arr.splice(i, 1)
-      if (arr.length === 0) {
-        results.push(memo.concat(cur))
-      }
-      permute(arr.slice(), memo.concat(cur))
-      arr.splice(i, 0, cur[0])
+function permutator(permutation) {
+  var length = permutation.length,
+    result = [permutation.slice()],
+    c = new Array(length).fill(0),
+    i = 1,
+    k,
+    p
+
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i]
+      p = permutation[i]
+      permutation[i] = permutation[k]
+      permutation[k] = p
+      ++c[i]
+      i = 1
+      result.push(permutation.slice())
+    } else {
+      c[i] = 0
+      ++i
     }
-
-    return results
   }
-  return permute(inputArr)
+  return result
 }
 
 module.exports = { permutator }
--- test/permutator/permutator.spec.js
+++ test/permutator/permutator.spec.js
@@ -1,15 +1,36 @@
+const _ = require('lodash')
 const { permutator } = require('../../src')
 
 describe('permutator', () => {
   it('should return 2 possible combinations of "a" and "b"', () => {
     const input = ['a', 'b']
-    const result = permutator(input)
-    expect(result.length).toBe(2)
+    const results = permutator(input)
+    expect(results.length).toBe(2)
   })
 
   it('should return 24 possible combinations of 4 distinct numbers', () => {
     const input = [1, 2, 3, 4]
-    const result = permutator(input)
-    expect(result.length).toBe(24)
+    const results = permutator(input)
+    expect(results.length).toBe(24)
+  })
+
+  it('should return the 6 possible combinations of 3 diferent letters', () => {
+    const input = ['a', 'b', 'c']
+    const results = permutator(input)
+    const expectedResults = [
+      ['a', 'b', 'c'],
+      ['a', 'c', 'b'],
+      ['b', 'a', 'c'],
+      ['b', 'c', 'a'],
+      ['c', 'a', 'b'],
+      ['c', 'b', 'a'],
+    ]
+    for (let result of results) {
+      expect(
+        expectedResults.some((expectedResult) =>
+          _.isEqual(expectedResult, result)
+        )
+      ).toBeTruthy()
+    }
   })
 })
